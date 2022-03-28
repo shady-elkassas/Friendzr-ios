510b076e623836ff9f8f6c2b83f70a7f8808b82d
Muhammed Sabri <85927201+muhammedsabrisaad@users.noreply.github.com>
2022-03-28 17:33:13 +0200

    fixed bugs

--- Friendzr/Modules/Events/Controller/ExpandDescriptionVC.swift
+++ Friendzr/Modules/Events/Controller/ExpandDescriptionVC.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 class ExpandDescriptionVC: UIViewController {
-
     
     @IBOutlet weak var dismissBtn: UIButton!
     @IBOutlet weak var hideView: UIView!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var discView: UIView!
     
+    @IBOutlet weak var viewHeightLayoutConstraint: NSLayoutConstraint!
+    @IBOutlet weak var closeBtn: UIButton!
     var myString : String = ""
     
     override func viewDidLoad() {
@@ -26,10 +27,36 @@ class ExpandDescriptionVC: UIViewController {
         self.title = "Event Description"
         
         textView.text = myString
+        
+//        let height = myString.height(withConstrainedWidth: discView.frame.width, font: UIFont(name: "Montserrat-Medium", size: 14)!)
+//
+//        viewHeightLayoutConstraint.constant = height + 20
+        
+        closeBtn.tintColor = .white
+        closeBtn.cornerRadiusForHeight()
     }
     
     @IBAction func dismissBtn(_ sender: Any) {
         self.onDismiss()
     }
     
+    @IBAction func closeBtn(_ sender: Any) {
+        self.onDismiss()
+    }
+}
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+    
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+
+        return ceil(boundingBox.width)
+    }
 }
